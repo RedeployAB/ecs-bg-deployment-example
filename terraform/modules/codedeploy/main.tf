4a4d168ea5bbeea6ae84863e01c66f66aa8594f1
Emiliano Sgrano <sgrilux@gmail.com>
2024-04-17 17:22:25 +0200

    fix codedeploy passrole policy

--- terraform/modules/codedeploy/main.tf
+++ terraform/modules/codedeploy/main.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "current" {}
+
 data "aws_iam_policy_document" "codedeploy" {
   statement {
     sid    = "S3Polocy"
@@ -39,7 +41,7 @@ data "aws_iam_policy_document" "codedeploy" {
     actions = [
       "iam:PassRole"
     ]
-    resources = ["*"]
+    resources = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.service_name}-task-execution-role"]
   }
 }
 
